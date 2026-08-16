b3f8719ebc5ba2100943719532252dc7bbe33069
Brian Zheng <zhengbrian1@gmail.com>
2026-08-16 16:00:53 -0400

    Add step() and clear_grad()

---
+++ optim.py
@@ -0,0 +1,16 @@
+from helpers import *
+
+class Optimizer:
+    def __init__(self, lr, batch_size, weight_decay = None):
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.batch_size = batch_size
+
+    def __call__(self, layer): # update the parameters
+        # print(f"{len(layer.W)}x{len(layer.W[0])}, {len(layer.W_grad)}x{len(layer.W_grad[0])}")
+        if self.weight_decay is not None:
+            layer.W = matrix_scalar_mult(1-self.weight_decay, layer.W)
+
+        layer.W = matrix_sub(layer.W, matrix_scalar_mult(self.lr/self.batch_size, layer.W_grad))
+        layer.b = matrix_sub(layer.b, matrix_scalar_mult(self.lr/self.batch_size, layer.b_grad))
+
